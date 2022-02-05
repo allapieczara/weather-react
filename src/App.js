506492b4460d3e952236ec64084947c397394d97
Alla <alla.pieczara@gmail.com>
2022-02-05 16:15:45 +0000

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import "./Weather.css";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultCity="Tokyo" />
+      <footer>
+        <a
+          href="https://github.com/allapieczara/weather-react.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Alla pieczara
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
 import Forecast from "./Forecast";
 import "./Forecast.css";
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="app-wrapper">
-        <div className="app-body">
-          <header>
-            <form className="d-flex">
-              <input
-                placeholder="Type your city in here"
-                className="form-control rounded"
-                type="search"
-                aria-label="Search"
-                id="city-search"
-              />
-              <button className="btn btn-outline-primary" type="submit">
-                Search
-              </button>
-              <button
-                className="btn btn-outline-success"
-                id="current-btn"
-                type="button"
-              >
-                Current
-              </button>
-            </form>
-          </header>
-          <main>
-            <div className="container">
-              <h1>Tokyo</h1>
-              <p className="today-date">Mon 31 Jan</p>
-              <div className="row">
-                <div className="col">
-                  <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="#ffd608"
-                    size={170}
-                    animate={true}
-                  />
-                </div>
-                <div className="col">
-                  <p className="description">Clear</p>
 
-                  <p className="bottom-margin">
-                    Wind: <span>5</span>Km/h
-                  </p>
-                  <p>
-                    Humidity: <span>81</span>%
-                  </p>
-                  <span>
-                    <h3 className="display-2 fw-bolder">12</h3>
-                    <div className="weather-temp">
-                      <h3>
-                        <p className="celcius">°C</p>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: "Mon 07 Feb",
+      icon: (
+        <ReactAnimatedWeather
+          icon="CLEAR_DAY"
+          color="#ffd608"
+          size={170}
+          animate={true}
+        />
+      ),
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="app-wrapper">
+          <div className="app-body">
+            <header>
+              <form className="d-flex">
+                <input
+                  placeholder="Type your city in here"
+                  className="form-control rounded"
+                  type="search"
+                  aria-label="Search"
+                  id="city-search"
+                  autoFocus="on"
+                />
+                <button className="btn btn-outline-primary" type="submit">
+                  Search
+                </button>
+                <button
+                  className="btn btn-outline-success"
+                  id="current-btn"
+                  type="button"
+                >
+                  Current
+                </button>
+              </form>
+            </header>
+            <main>
+              <div className="container">
+                <h1>{weatherData.city}</h1>
+                <p className="today-date">{weatherData.date}</p>
+                <div className="row">
+                  <div className="col">{weatherData.icon}</div>
+                  <div className="col">
+                    <p className="description">{weatherData.description}</p>
+
+                    <p className="bottom-margin">
+                      Wind: <span>{Math.round(weatherData.wind)}</span> Km/h
+                    </p>
+                    <p>
+                      Humidity: <span>{weatherData.humidity}</span>%
+                    </p>
+                    <span>
+                      <h3 className="display-2 fw-bolder">
+                        {Math.round(weatherData.temperature)}
                       </h3>
-                    </div>
-                  </span>
+                      <div className="weather-temp">
+                        <h3>
+                          <p className="celcius">°C</p>
+                        </h3>
+                      </div>
+                    </span>
+                  </div>
+                </div>
+                <div className="weather-forecast">
+                  <Forecast />
                 </div>
               </div>
-              <div className="weather-forecast">
-                <Forecast />
-              </div>
-            </div>
-          </main>
+            </main>
+          </div>
         </div>
       </div>
-      <footer>
-        <a
-          href="https://github.com/allapieczara/weather-react.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Alla pieczara
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    let metric = "&units=metric";
+   
+    const apiKey = "9505a4340fe19568106c1c5b7dc42321";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}${metric}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
